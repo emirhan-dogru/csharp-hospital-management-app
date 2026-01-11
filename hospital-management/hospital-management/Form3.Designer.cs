4ca0e8b42355addc05bd6209d1df8e30318de287
Emirhan Doğru <emirhan-dogru@hotmail.com>
2026-01-11 17:00:07 +0000

    hasta güncelleme sayfası eklendi

--- hospital-management/hospital-management/Form3.Designer.cs
+++ hospital-management/hospital-management/Form3.Designer.cs
@@ -28,12 +28,290 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hasta Güncelle";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(43, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 569);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hasta Listesi";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 46);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(302, 517);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(383, 102);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(519, 569);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hasta Bilgileri";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(357, 511);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 39);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Kapat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 511);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 39);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Güncelle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(185, 463);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(298, 31);
+            this.dateTimePicker2.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(25, 467);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(129, 29);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Taburcu:";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(185, 409);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(98, 411);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Yaş:";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(335, 359);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(98, 29);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Kadın";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(185, 361);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(98, 29);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Erkek";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Cinsiyet:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(185, 295);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(298, 31);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Yatış Tarihi:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(185, 179);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(298, 90);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Teşhis:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(185, 123);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(298, 33);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hastalık Türü:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(185, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ad Soyad:";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form3";
+            this.ClientSize = new System.Drawing.Size(914, 779);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Hasta Güncelle";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
--- hospital-management/hospital-management/Form3.cs
+++ hospital-management/hospital-management/Form3.cs
@@ -7,14 +7,178 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace hospital_management
 {
     public partial class Form3 : Form
     {
+        SqlConnection baglan = new SqlConnection("Server=63.178.197.220,1433;Database=hastane;User Id=dbuser;Password=1234;TrustServerCertificate=True;");
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            HastalikTuruGetir();
+            HastalariGetir();
+        }
+
+        void HastalariGetir()
+        {
+            da = new SqlDataAdapter("SELECT Id, AdSoyad FROM Hasta", baglan);
+            ds = new DataSet();
+            baglan.Open();
+            da.Fill(ds, "Hasta");
+            dataGridView1.DataSource = ds.Tables["Hasta"];
+            baglan.Close();
+
+            dataGridView1.Columns["Id"].Visible = false;
+            dataGridView1.Columns["AdSoyad"].HeaderText = "Ad Soyad";
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        void HastalikTuruGetir()
+        {
+            baglan.Open();
+            SqlDataAdapter da2 = new SqlDataAdapter("SELECT Id, HastalikAdi FROM HastalikTuru", baglan);
+            DataTable dt = new DataTable();
+            da2.Fill(dt);
+            baglan.Close();
+
+            comboBox1.DataSource = dt;
+            comboBox1.DisplayMember = "HastalikAdi";
+            comboBox1.ValueMember = "Id";
+            comboBox1.SelectedIndex = -1;
+        }
+
+        // datagridviewdan hasta secildiginde
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+
+            int secilenId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Id"].Value);
+
+            baglan.Open();
+            SqlCommand HastaBilgi = new SqlCommand("SELECT * FROM Hasta WHERE Id=@id", baglan);
+            HastaBilgi.Parameters.AddWithValue("@id", secilenId);
+            SqlDataReader dr = HastaBilgi.ExecuteReader();
+
+            if (dr.Read())
+            {
+                textBox1.Text = dr["AdSoyad"].ToString();
+                comboBox1.SelectedValue = dr["HastalikTuruId"];
+                textBox2.Text = dr["TeshisDetayi"].ToString();
+                dateTimePicker1.Value = Convert.ToDateTime(dr["YatisTarihi"]);
+
+                int cinsiyet = Convert.ToInt32(dr["Cinsiyet"]);
+                if (cinsiyet == 0)
+                {
+                    radioButton1.Checked = true;
+                }
+                else
+                {
+                    radioButton2.Checked = true;
+                }
+
+                numericUpDown1.Value = Convert.ToInt32(dr["Yas"]);
+
+                if (dr["TaburcuTarihi"] != DBNull.Value)
+                {
+                    checkBox1.Checked = true;
+                    dateTimePicker2.Value = Convert.ToDateTime(dr["TaburcuTarihi"]);
+                    dateTimePicker2.Enabled = true;
+                }
+                else
+                {
+                    checkBox1.Checked = false;
+                    dateTimePicker2.Enabled = false;
+                    dateTimePicker2.Text = null;
+                }
+            }
+
+            dr.Close();
+            baglan.Close();
+        }
+
+        // guncelle butonu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Hasta seçiniz!");
+                return;
+            }
+
+            int secilenId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+
+            baglan.Open();
+
+            string guncelle;
+            if (checkBox1.Checked)
+            {
+                guncelle = "UPDATE Hasta SET AdSoyad=@adsoyad, HastalikTuruId=@hastalikturuId, TeshisDetayi=@teshis, YatisTarihi=@yatisTarihi, Cinsiyet=@cinsiyet, Yas=@yas, TaburcuTarihi=@taburcuTarihi WHERE Id=@id";
+            }
+            else
+            {
+                guncelle = "UPDATE Hasta SET AdSoyad=@adsoyad, HastalikTuruId=@hastalikturuId, TeshisDetayi=@teshis, YatisTarihi=@yatisTarihi, Cinsiyet=@cinsiyet, Yas=@yas, TaburcuTarihi=NULL WHERE Id=@id";
+            }
+
+            SqlCommand VeriGuncelle = new SqlCommand(guncelle, baglan);
+            VeriGuncelle.Parameters.AddWithValue("@id", secilenId);
+            VeriGuncelle.Parameters.AddWithValue("@adsoyad", textBox1.Text);
+            VeriGuncelle.Parameters.AddWithValue("@hastalikturuId", comboBox1.SelectedValue);
+            VeriGuncelle.Parameters.AddWithValue("@teshis", textBox2.Text);
+            VeriGuncelle.Parameters.AddWithValue("@yatisTarihi", dateTimePicker1.Value);
+            VeriGuncelle.Parameters.AddWithValue("@cinsiyet", radioButton1.Checked ? 0 : 1);
+            VeriGuncelle.Parameters.AddWithValue("@yas", numericUpDown1.Value);
+
+            if (checkBox1.Checked)
+            {
+                VeriGuncelle.Parameters.AddWithValue("@taburcuTarihi", dateTimePicker2.Value);
+            }
+
+            VeriGuncelle.ExecuteNonQuery();
+            baglan.Close();
+
+            MessageBox.Show("Veri Güncellendi.");
+            HastalariGetir();
+            Temizle();
+        }
+
+        // taburcu checkbox
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            dateTimePicker2.Enabled = checkBox1.Checked;
+            if (checkBox1.Checked)
+            {
+                dateTimePicker2.Value = DateTime.Now;
+            }
+        }
+
+        void Temizle()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            comboBox1.SelectedIndex = -1;
+            dateTimePicker1.Value = DateTime.Now;
+            radioButton1.Checked = true;
+            numericUpDown1.Value = 1;
+            checkBox1.Checked = false;
+            dateTimePicker2.Enabled = false;
+        }
+
+        // kapat butonu
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
